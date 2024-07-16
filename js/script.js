f4dca37ee725be665bafd460162f824cc6cae576
Carlos <dogmalcarlos@gmail.com>
2024-07-16 04:24:32 -0300

    Modificando a lógica de criptografia e ajustando alguns detalhes no layout

--- js/script.js
+++ js/script.js
@@ -1,88 +1,143 @@
 const inputTexto = document.querySelector(".input-texto");
 const mensagem = document.querySelector(".mensagem");
-const inputOriginalOculto = document.querySelector('.input-original-oculto');
 const erro = document.querySelector(".erro");
 
-inputTexto.addEventListener('input', validarEntrada);
+let matrizCodigoEncriptar = {
+  e: "enter",
+  i: "imes",
+  a: "ai",
+  o: "ober",
+  u: "ufat",
+};
+
+const matrizCodigoDescriptografia = {
+  ai: "a",
+  ober: "o",
+  imes: "i",
+  enter: "e",
+  ufat: "u",
+};
+
+let textoCriptografado = "";
+
+inputTexto.addEventListener("input", validarEntrada);
 
 function validarEntrada() {
-    const texto = inputTexto.value;
-    const regex = /^[a-z\s]*$/;
+  const texto = inputTexto.value;
+  const regex = /^[a-z\s]*$/;
 
-    if (!regex.test(texto)) {
-        erro.textContent = "Somente letras minúsculas e palavras sem acento";
-        inputTexto.value = texto.replace(/[^a-z\s]/g, '');
-    } else {
-        erro.textContent = "";
-    }
+  if (!regex.test(texto)) {
+    erro.textContent = "Somente letras minúsculas e palavras sem acento";
+    inputTexto.value = texto.replace(/[^a-z\s]/g, "");
+  } else {
+    erro.textContent = "";
+  }
 }
 
 function btnEncriptar() {
-    const textoEncriptado = encriptar(inputTexto.value);
-    mensagem.value = textoEncriptado;
-    mensagem.style.backgroundImage = "none";
+  const textoEncriptado = encriptar(inputTexto.value);
+  mensagem.value = textoEncriptado;
+  mensagem.style.backgroundImage = "none";
 
-    inputOriginalOculto.value = inputTexto.value;
+  inputTexto.value = inputTexto.value;
 }
 
 function encriptar(stringEncriptada) {
-    let matrizCodigo = [["e", "shit"], ["i", "imes"], ["a", "aiui"], ["o", "ober"], ["u", "ufat"]];
-    stringEncriptada = stringEncriptada.toLowerCase();
+  stringEncriptada = stringEncriptada.toLowerCase();
 
-    for (let i = 0; i < matrizCodigo.length; i++) {
-        if (stringEncriptada.includes(matrizCodigo[i][0])) {
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
-        }
+  for (let i = 0; i < stringEncriptada.length; i++) {
+    if (matrizCodigoEncriptar.hasOwnProperty(stringEncriptada[i])) {
+      textoCriptografado += matrizCodigoEncriptar[stringEncriptada[i]];
+    } else {
+      textoCriptografado += stringEncriptada[i];
     }
+  }
 
-    return stringEncriptada;
+  return textoCriptografado;
 }
 
 function btnDesencriptar() {
-    const textoDesencriptado = desencriptar(inputOriginalOculto.value);
-    mensagem.value = textoDesencriptado;
-    mensagem.style.backgroundImage = "none";
+  const textoDesencriptado = desencriptar(inputTexto.value);
+
+  mensagem.value = textoDesencriptado;
+  mensagem.style.backgroundImage = "none";
 }
 
 function desencriptar(stringDesencriptada) {
-    let matrizCodigo = [["e", "shit"], ["i", "imes"], ["a", "aiui"], ["o", "ober"], ["u", "ufat"]];
-    stringDesencriptada = stringDesencriptada.toLowerCase();
+  stringDesencriptada = stringDesencriptada.toLowerCase();
 
-    for (let i = 0; i < matrizCodigo.length; i++) {
-        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
-            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
-        }
+  let textoCriptografado = [];
+
+  for (let i = 0; i < stringDesencriptada.length; i++) {
+    if (
+      matrizCodigoDescriptografia.hasOwnProperty(
+        stringDesencriptada.substr(i, 5)
+      )
+    ) {
+      textoCriptografado +=
+        matrizCodigoDescriptografia[stringDesencriptada.substr(i, 5)];
+      i = i + 4;
+    } else if (
+      matrizCodigoDescriptografia.hasOwnProperty(
+        stringDesencriptada.substr(i, 4)
+      )
+    ) {
+      textoCriptografado +=
+        matrizCodigoDescriptografia[stringDesencriptada.substr(i, 4)];
+      i = i + 3;
+    } else if (
+      matrizCodigoDescriptografia.hasOwnProperty(
+        stringDesencriptada.substr(i, 3)
+      )
+    ) {
+      textoCriptografado +=
+        matrizCodigoDescriptografia[stringDesencriptada.substr(i, 3)];
+      i = i + 2;
+    } else if (
+      matrizCodigoDescriptografia.hasOwnProperty(
+        stringDesencriptada.substr(i, 2)
+      )
+    ) {
+      textoCriptografado +=
+        matrizCodigoDescriptografia[stringDesencriptada.substr(i, 2)];
+      i = i + 1;
+    } else {
+      textoCriptografado += stringDesencriptada[i];
     }
+  }
 
-    return stringDesencriptada;
+  return textoCriptografado;
 }
 
 function btnCopiar() {
-    var mensagem_criptografada = document.querySelector('.mensagem');
-    var campo_mensagem_original = document.querySelector('.input-texto');
+  var mensagem_criptografada = document.querySelector(".mensagem");
+  var campo_mensagem_original = document.querySelector(".input-texto");
 
-    campo_mensagem_original.value = mensagem_criptografada.value;
+  campo_mensagem_original.value = mensagem_criptografada.value;
+
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(mensagem_criptografada.value).catch((err) => {
+      alert("Erro ao copiar o texto:", err);
+    });
+  }
 }
 
 function btnLimpar() {
-    var mensagem_criptografada = document.querySelector('.mensagem');
-    var campo_mensagem_original = document.querySelector('.input-texto');
+  var mensagem_criptografada = document.querySelector(".mensagem");
+  var campo_mensagem_original = document.querySelector(".input-texto");
 
-    campo_mensagem_original.value = "";
-    mensagem_criptografada.value = "";
+  campo_mensagem_original.value = "";
+  mensagem_criptografada.value = "";
 }
-
-
-
-
-
-
 
 const text = "DECODIFICADOR";
 const title = document.getElementById("title");
 
 for (let char of text) {
-    const span = document.createElement("span");
-    span.textContent = char;
-    title.appendChild(span);
+  const span = document.createElement("span");
+  span.textContent = char;
+  title.appendChild(span);
 }
+
+inputTexto.innerText = inputTexto.value.trim();
+mensagem.innerText = inputTexto.value.trim();
